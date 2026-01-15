2060f7f338b6ae5a1f093bdd39c08b475c905c26
T-SMod <Tagir-Studio@yandex.ru>
2026-01-15 17:18:10 +0300

    Update modconsole.js

--- assets/js/libs/modconsole.js
+++ assets/js/libs/modconsole.js
@@ -1,5 +1,5 @@
 const alllteLog = console
 let TSNuxConsole = []
 console = {
-  "log": function(a) { alllteLog.log(a), TSNuxConsole.push({ "type": "log", "text": a }) }
+  "log": function(a) { alllteLog.log(a), TSNuxConsole.push({ "type": "log", "text": a }) },
 }
